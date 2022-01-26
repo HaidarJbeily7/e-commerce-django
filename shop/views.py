c5001bcd77b62f902bfe6ca00389143e3a6ab31f
Haidar Jbeily <haidar@sellanyad.de>
2022-01-26 04:25:13 +0200

    Feat (Template, View): add the search functionality

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
     products = Product.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('prod_title')
+        if title != '' and title is not None:
+            products = products.filter(title__icontains = title)
 
+    paginator = Paginator(products, 4)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
     return render(request, 'shop/index.html', {"products" : products})
