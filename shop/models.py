a44f64ed5bc7c61f2dc31400caa91273cb92de28
Haidar Jbeily <haidar@sellanyad.de>
2022-01-26 22:36:38 +0200

    Add the checkout functionality to the backend

--- shop/models.py
+++ shop/models.py
@@ -11,3 +11,14 @@ class Product(models.Model):
 
     class Meta:
         db_table = 'products'
+
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    email =models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    total = models.CharField(max_length=200)
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from numpy import product
-from .models import Product
+from .models import Product, Order
 from django.core.paginator import Paginator
 # Create your views here.
 
@@ -18,5 +18,21 @@ def index(request):
     return render(request, 'shop/index.html', {"products" : products})
 
 def details(request, id):
-    product = get_object_or_404(Product, id=id)
-    return render(request, 'shop/details.html', {"product":product})
+    product_object = get_object_or_404(Product, id=id)
+    return render(request, 'shop/details.html', {"product_object":product_object})
+
+def checkout(request):
+
+    if request.method == "POST":
+        items = request.POST.get('items','')
+        name = request.POST.get('name',"")
+        email = request.POST.get('email',"")
+        address = request.POST.get('address',"")
+        city = request.POST.get('city',"")
+        state =request.POST.get('state',"")
+        zipcode = request.POST.get('zipcode',"")
+        total = request.POST.get('total',"")
+        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
+        order.save()
+
+    return render(request,'shop/checkout.html')
